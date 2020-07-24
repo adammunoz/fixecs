714c6d8e4c5bc07b030989fc85dd5244cf126f4e
Adam Munoz <adam.munoz.lopez@gmail.com>
2020-07-24 21:44:51 +0900

    spec

--- project.clj
+++ project.clj
@@ -2,5 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/test.check "0.10.0"]
                  [cheshire "5.10.0"]]
+
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.632"]]}}
+
+  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :main fixecs.core)
--- src/fixecs/core.clj
+++ src/fixecs/core.clj
@@ -1,24 +1,49 @@
 (ns fixecs.core
  (:gen-class)
- (:require [cheshire.core :refer :all]
+ (:require [clojure.spec.alpha :as s]
+           [cheshire.core :refer :all]
            [clojure.java.io :as io]))
+
+; Begin Data Specs
+
+(s/def ::name string?)
+(s/def ::env-var (s/keys :req-un [::name]))
+(s/def ::environment (s/coll-of ::env-var))
+
+(s/def ::ecs-task (s/map-of 
+ #{"environment"} ::environment
+ :min-count 1))
+
+; End Data Specs
 
 (defn parse-json [path] 
  (-> path
      slurp
      parse-string))
 
+(s/fdef sort-env
+ :args (s/cat :j-from-file ::ecs-task)
+ :ret ::environment)
+
 (defn sort-env [j-from-file]
   (sort-by #(% "name")
            (j-from-file "environment")))
 
+(s/fdef merge-env
+ :args (s/cat :j-original ::ecs-task
+              :j-env ::environment)
+ :ret ::ecs-task)
+
 (defn merge-env [j-original j-env]
  (merge j-original {"environment" j-env}))
+
+(s/fdef convert
+ :args (s/cat :original ::ecs-task)
+ :ret ::ecs-task)
 
 (defn convert [original]
  (merge-env original
             (sort-env original)))
-
 
 (defn -main [& args]
  (let [path (first args)]
